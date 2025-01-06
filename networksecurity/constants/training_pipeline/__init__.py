e1832806d139f6a6b073dc79dc7d977564840fc4
Harsh Shah <harshshah37@Harshs-MacBook-Air.local>
2025-01-05 23:00:06 -0500

    Data Validation Completed

--- networksecurity/constants/training_pipeline/__init__.py
+++ networksecurity/constants/training_pipeline/__init__.py
@@ -4,12 +4,14 @@ import numpy as np
 import pandas as pd
 
 TARGET_COLUMN = "Result"
-PIPELINE_NAME: str = "NetworkSec"
+PIPELINE_NAME: str = "NetworkSecurityPipeline"
 ARTIFACT_DIR: str = "Artifacts"
 FILE_NAME: str = "phishingData.csv"
 
 TRAIN_FILE_NAME: str = "train.csv"
 TEST_FILE_NAME: str = "test.csv"
+
+SCHEMA_FILE_PATH = os.path.join("data_schema", "schema.yaml")
 
 DATA_INGESTION_COLLECTION_NAME: str = "NetworkData"
 DATA_INGESTION_DATABASE_NAME: str = "NetworkSecurity"
@@ -17,3 +19,10 @@ DATA_INGESTION_DIR_NAME: str = "data_ingestion"
 DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
 DATA_INGESTION_INGESTED_DIR: str = "ingested"
 DATA_INGESTION_TRAIN_TEST_SPLIT_RATION: float = 0.2
+
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+DATA_VALIDATION_VALID_DIR: str = "validated"
+DATA_VALIDATION_INVALID_DIR: str = "invalid"
+DATA_VALIDATION_DRIFT_REPORT_DIR: str = "drift_report"
+DATA_VALIDATION_DRIFT_REPORT_FILE_NAME: str = "report.yaml"
+PREPROCESSING_OBJECT_FILE_NAME = "preprocessing.pkl"
