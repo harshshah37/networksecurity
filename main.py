a2522e7e3126d3a1744cb3142e39364bc2282722
Harsh Shah <harshshah37@Harshs-MacBook-Air.local>
2025-01-06 21:22:39 -0500

    Training Pipeline w/ MLFlow, DagsHub & S3 Implementation

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from networksecurity.components.data_transformation import DataTransformation
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
 from networksecurity.entity.config_entity import DataIngestionConfig, TrainingPipelineConfig, DataValidationConfig, DataTransformationConfig
-
+from networksecurity.components.model_trainer import ModelTrainer
+from networksecurity.entity.config_entity import ModelTrainerConfig
 import sys
 
 if __name__ == '__main__':
@@ -31,6 +32,13 @@ if __name__ == '__main__':
         data_transformation_artifact = data_transformation.initiate_data_transformation()
         print(data_transformation_artifact)
         logging.info("data Transformation completed")
+
+        logging.info("Model Training sstared")
+        model_trainer_config = ModelTrainerConfig(trainingpipelineconfig)
+        model_trainer = ModelTrainer(model_trainer_config = model_trainer_config, data_transformation_artifact = data_transformation_artifact)
+        model_trainer_artifact = model_trainer.initiate_model_trainer()
+
+        logging.info("Model Training artifact created")
 
     except Exception as e:
         raise NetworkSecurityException(e, sys)
--- networksecurity/utils/main_utils/utils.py
+++ networksecurity/utils/main_utils/utils.py
@@ -56,47 +56,38 @@ def load_object(file_path: str, ) -> object:
     except Exception as e:
         raise NetworkSecurityException(e, sys) from e
     
-# def load_numpy_array_data(file_path: str) -> np.array:
-#     """
-#     load numpy array data from file
-#     file_path: str location of file to load
-#     return: np.array data loaded
-#     """
-#     try:
-#         with open(file_path, "rb") as file_obj:
-#             return np.load(file_obj)
-#     except Exception as e:
-#         raise NetworkSecurityException(e, sys) from e
+def load_numpy_array_data(file_path: str) -> np.array:
+    try:
+        with open(file_path, "rb") as file_obj:
+            return np.load(file_obj)
+    except Exception as e:
+        raise NetworkSecurityException(e, sys) from e
     
+def evaluate_models(X_train, y_train, X_test, y_test, models, param):
+    try:
+        report = {}
 
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            para=param[list(models.keys())[i]]
 
-# def evaluate_models(X_train, y_train,X_test,y_test,models,param):
-#     try:
-#         report = {}
+            gs = GridSearchCV(model, para, cv = 3)
+            gs.fit(X_train, y_train)
 
-#         for i in range(len(list(models))):
-#             model = list(models.values())[i]
-#             para=param[list(models.keys())[i]]
+            model.set_params(**gs.best_params_)
+            model.fit(X_train, y_train)
 
-#             gs = GridSearchCV(model,para,cv=3)
-#             gs.fit(X_train,y_train)
+            y_train_pred = model.predict(X_train)
 
-#             model.set_params(**gs.best_params_)
-#             model.fit(X_train,y_train)
+            y_test_pred = model.predict(X_test)
 
-#             #model.fit(X_train, y_train)  # Train model
+            train_model_score = r2_score(y_train, y_train_pred)
 
-#             y_train_pred = model.predict(X_train)
+            test_model_score = r2_score(y_test, y_test_pred)
 
-#             y_test_pred = model.predict(X_test)
+            report[list(models.keys())[i]] = test_model_score
 
-#             train_model_score = r2_score(y_train, y_train_pred)
+        return report
 
-#             test_model_score = r2_score(y_test, y_test_pred)
-
-#             report[list(models.keys())[i]] = test_model_score
-
-#         return report
-
-#     except Exception as e:
-#         raise NetworkSecurityException(e, sys)
+    except Exception as e:
+        raise NetworkSecurityException(e, sys)
